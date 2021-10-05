8f5dd1077a4d6ae9e18050de2a9631425ee08c00
Valeria.Shelgunova <Valeria.Shelgunova@techinput.ru>
2021-10-05 09:21:56 +0300

    Changed job-is-done window. Fixed bug of empty or nonexistent args.txt. Added js-error alert window. Continue changing the interface.

--- js_code/event_listeners.js
+++ js_code/event_listeners.js
@@ -1,5 +1,5 @@
 const { PythonShell } = require('python-shell')
-const fs = require('fs')
+const fs = require('fs-extra')
 const constants = require('./constants_js')
 const utils = require('./utils')
 const uploadHelpers = require('./upload_file_functions')
@@ -12,11 +12,18 @@ const options = {
 
 const buttonFileCycle = document.getElementById('go')
 buttonFileCycle.addEventListener('click', () => {
-  const textOpt = fs.readFileSync(`${constants.PATHS.utilsPath}options.args.txt`, 'utf8')
+
+  if (!fs.existsSync(`${constants.PATHS.utilsPath}options.args.txt`)) {
+    utils.logToFile("File options.args.txt doesn't exist")
+    return
+  }
+
+  const textOpt = fs.readFileSync(`${constants.PATHS.utilsPath}options.args.txt`, 'utf8');
+  if (textOpt.length === 0) {return}
   utils.logToElectron(`TEXT_OPT: ${textOpt}`)
   const textByLine = textOpt.split('?')
-  for (const strr of textByLine) {
-    options.args.push(strr)
+  for (const str of textByLine) {
+    options.args.push(str)
   }
 
   PythonShell.run(path.join(__dirname, '../python_code/entry_python_point.py'), options, (err, results) => {
@@ -28,15 +35,17 @@ buttonFileCycle.addEventListener('click', () => {
       utils.logToElectron(`PYTHONSHELLRUN ERROR: ${err}`)
     }
     document.getElementById('reset').style.display = 'inline'
+    document.getElementById('first_line').style.display = 'inline'
+    document.getElementById('second_line').style.display = 'inline'
+    document.getElementById('address').style.display = 'inline'
+    document.getElementById('address').innerHTML = constants.PATHS.completePath;
+
     document.body.style.cursor = 'auto'
     document.getElementById('wait').style.display = 'none'
 
     utils.logToElectron(results)
     utils.logToElectron(`RESULT: ${results}`)
   })
-})
-
-buttonFileCycle.addEventListener('click', () => {
   document.getElementById('leftbuttons').style.display = 'none'
   document.getElementById('inputLabel').style.display = 'none'
   document.getElementById('rightbox').style.display = 'none'
@@ -46,6 +55,8 @@ buttonFileCycle.addEventListener('click', () => {
   document.body.style.cursor = 'wait'
   document.getElementById('wait').style.display = 'inline'
 })
+
+
 const buttonReset = document.getElementById('reset')
 
 buttonReset.addEventListener('click', () => {
@@ -56,7 +67,15 @@ buttonReset.addEventListener('click', () => {
   document.getElementById('leftbuttons').style.display = 'inline'
   document.getElementById('go').style.display = 'inline'
   document.getElementById('reset').style.display = 'none'
+  document.getElementById('first_line').style.display = 'none'
+  document.getElementById('second_line').style.display = 'none'
+  document.getElementById('address').style.display = 'none'
   options.args.length = 0
   uploadHelpers.arrayFiles.length = 0
   fs.writeFileSync(`${constants.PATHS.utilsPath}options.args.txt`, '')
+})
+
+window.addEventListener("error", function (e) {
+  alert("Error occurred: " + e.error.message);
+  return false;
 })
